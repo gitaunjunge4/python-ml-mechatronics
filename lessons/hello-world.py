9cb28f47b52617994b9caf55d1b9b0ec1510a4ac
Njung'e Gitau <gitaunjunge4@gmail.com>
2025-08-21 20:50:07 +0300

    Day 1 commit

--- lessons/hello-world.py
+++ lessons/hello-world.py
@@ -1,8 +1,26 @@
-# Example: Casting data types
-float_result = float("42")
-integer_from_float_result = int(float("3.14"))
-integer_result = int("3.14")
-# Print the variable and its data type. 
-# Use colon (:) as separator 
-print(float_result,":", type(float_result))
-print(integer_from_float_result, ":", type(integer_from_float_result))
+# Day 1 
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Gitau'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Gitau'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+# Find an Euclidian distance between (2, 3) and (10, 8)
+def pyTheorem(a, b):
+    result = (a**2 + b**2)**0.5
+    return result
+print(pyTheorem(2,3))
+print(pyTheorem(10,8))
