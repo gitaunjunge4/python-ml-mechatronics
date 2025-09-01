48ccfc5cedf4cfc249de2084a10aa04e81d95e77
Njung'e Gitau <gitaunjunge4@gmail.com>
2025-09-01 21:24:09 +0300

    Completed the list exercises

---
+++ lessons/lists_exercise.py
@@ -0,0 +1,18 @@
+# List: is a collection which is ordered and changeable(modifiable). Allows duplicate members.
+
+#Unpacking list items 
+lst = ['item1','item2','item3', 'item4', 'item5']
+first_item, second_item, third_item, *rest = lst
+print(first_item)     # item1
+print(second_item)    # item2
+print(third_item)     # item3
+print(rest)           # ['item4', 'item5']
+
+countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
+gr, fr, bg, sw, *scandic, es = countries
+print(gr)
+print(fr)
+print(bg)
+print(sw)
+print(scandic)
+print(es)
