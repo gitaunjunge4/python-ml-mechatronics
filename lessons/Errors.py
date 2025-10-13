b5871481b4217af3092d8b2a3cb405a6865d58d1
Njung'e Gitau <gitaunjunge4@gmail.com>
2025-10-13 16:00:06 +0300

    Errors and Exceptions

---
+++ lessons/Errors.py
@@ -0,0 +1,9 @@
+# syntax error -
+# name error - when a variable s not defined 
+# index error - when you give an index that is not in teh range
+# module not found error - when importing a module that is wrong ie in syntax and all
+# attribute error - when you call an atribute that the imported module doesn't have
+# key error - when nalling a non existent/ wrong key in an object 
+# type error - operating on data types that aren't similar 
+# import error - importing wrong function name 
+# value error - 
